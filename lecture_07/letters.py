570cb3c3214fe36148a894f0fce44de5046e5f75
Oliver Cordes <ocordes@astro.uni-bonn.de>
2023-05-19 10:06:52 +0200

    - add missing files

---
+++ lecture_07/letters.py
@@ -0,0 +1,14 @@
+# module letters.py
+
+# written by: your-name, date
+# changed by: your-name, date
+
+
+# wikipedia statistics
+
+wikipedia_stats_english = { 'e': 0.1270, 't': 0.09056, 'a': 0.08167, 'o': 0.07507, 'l': 0.06966, 'n' : 0.06749 }
+wikipedia_stats_german  = { 'e': 0.1740, 'n': 0.0978, 'l': 0.0755, 's' : 0.0727, 'r': 0.0700, 'a': 0.0651 } 
+wikipedia_stats_italian = { 'e': 0.1179, 'a': 0.1174, 'l': 0.1128, 'o' : 0.0983, 'n': 0.0688, 'l': 0.0651 } 
+
+
+# your solution here please
---
+++ lecture_07/logging_demo.py
@@ -0,0 +1,10 @@
+import logging
+
+#logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(filename='demo.log', format='%(asctime)s %(message)s', level=logging.DEBUG )
+
+logging.info('Useful informations!')
+logging.debug('Some debug notes!')
+logging.warning('Some warnings.')
+logging.error('Some errors.')
+logging.critical('Critical errors.')
