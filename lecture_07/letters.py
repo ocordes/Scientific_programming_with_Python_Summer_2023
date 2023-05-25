99b96acfca94c8e3bf2e3f4a871c31c3af57f72a
Oliver Cordes <ocordes@astro.uni-bonn.de>
2023-05-25 11:05:52 +0200

    - fix wrong submission date/letter frequencies

--- lecture_07/letters.py
+++ lecture_07/letters.py
@@ -7,8 +7,8 @@
 # wikipedia statistics
 
 wikipedia_stats_english = { 'e': 0.1270, 't': 0.09056, 'a': 0.08167, 'o': 0.07507, 'l': 0.06966, 'n' : 0.06749 }
-wikipedia_stats_german  = { 'e': 0.1740, 'n': 0.0978, 'l': 0.0755, 's' : 0.0727, 'r': 0.0700, 'a': 0.0651 } 
-wikipedia_stats_italian = { 'e': 0.1179, 'a': 0.1174, 'l': 0.1128, 'o' : 0.0983, 'n': 0.0688, 'l': 0.0651 } 
+wikipedia_stats_german  = { 'e': 0.1639, 'n': 0.0978, 's': 0.0727, 'r' : 0.0700, 'i': 0.0655, 'a': 0.0651 } 
+wikipedia_stats_italian = { 'e': 0.1179, 'a': 0.1174, 'i': 0.1128, 'o' : 0.0983, 'n': 0.0688, 'l': 0.0651 } 
 
 
 # your solution here please
